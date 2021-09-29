ebf1ff2ed7be65c728e198df0928eafb82cc2ffc
Adam Rutter <adamrutter92@gmail.com>
2021-09-29 14:31:00 +0100

    rebase initial

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,19 @@
-export declare function compress(string: string, options?: {
-  mode: number,
-  enableEndMark: boolean
-}): string
+export declare function compress(
+  string: string,
+  options?: {
+    mode: number;
+    enableEndMark: boolean;
+  }
+): string;
 
+export declare function compressUrlSafe(
+  string: string,
+  options?: {
+    mode: number;
+    enableEndMark: boolean;
+  }
+): string;
 
-export declare function compressUrlSafe(string: string, options?: {
-  mode: number,
-  enableEndMark: boolean
-}): string
+export declare function decompress(bytes: number[] | Int8Array): string;
 
-export declare function decompress(bytes: number[]|Int8Array): string
-
-export declare function decompressUrlSafe(string: string): string
+export declare function decompressUrlSafe(string: string): string;
